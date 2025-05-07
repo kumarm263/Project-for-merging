c3d8842d85b3ed65f716abd972d8c2ac71e07eed
kartikM2005 <kartikmagadum18@gmail.com>
2025-05-07 19:23:37 +0530

    updated add_student.c

--- add_student.c
+++ add_student.c
@@ -1,1 +1,19 @@
-//do it
+#include <stdio.h>
+#include "student.h"
+
+void addStudent() {
+    if (count >= MAX) {
+        printf("Cannot add more students. Limit reached.\n");
+        return;
+    }
+
+    printf("Enter Roll No: ");
+    scanf("%d", &students[count].roll);
+    printf("Enter Name: ");
+    scanf(" %[^\n]", students[count].name);
+    printf("Enter Marks: ");
+    scanf("%f", &students[count].marks);
+    
+    count++;
+    printf("Student added successfully.\n");
+}
