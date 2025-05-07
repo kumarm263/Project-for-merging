0a789be72abef7ed80a0324f6b8b2bd41a13f958
kumarm263 <kumarmadival263@gmail.com>
2025-05-07 19:49:33 +0530

    Updated  display_student.c

--- display_student.c
+++ display_student.c
@@ -1,1 +1,15 @@
-//add display function
+#include <stdio.h>
+#include "student.h"
+
+void viewStudents() {
+    if (count == 0) {
+        printf("No student records to display.\n");
+        return;
+    }
+
+    printf("\n--- Student List ---\n");
+    for (int i = 0; i < count; i++) {
+        printf("%d. Roll: %d | Name: %s | Marks: %.2f\n",
+               i + 1, students[i].roll, students[i].name, students[i].marks);
+    }
+}
